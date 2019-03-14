b7d3a0fbb48bd50fb446ee55d50f63de726a1c5e
wecode <umurangamclaire@gmail.com>
2019-03-14 18:54:10 +0200

    Debbuging

--- pictures/views.py
+++ pictures/views.py
@@ -3,23 +3,23 @@ from django.http import HttpResponse, Http404
 from .models import Image
 
 
-# def search_results(request):
+def search_results(request):
 
-#     if 'image' in request.GET and request.GET["image"]:
-#         search_term = request.GET.get("image")
-#         searched_images = Image.search_by_title(search_term)
-#         message = f"{search_term}"
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_title(search_term)
+        message = f"{search_term}"
 
-#         return render(request, 'all-news/search.html',{"message":message,"images": searched_images})
+        return render(request, 'all-news/search.html',{"message":message,"images": searched_images})
 
-#     else:
-#         message = "You haven't searched for any term"
-#         return render(request, 'search.html',{"message":message})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
 
-# def image(request,image_id):
+def image(request,image_id):
     
-#     try:
-#         image = Image.objects.get(id = image_id)
-#     except DoesNotExist:
-#         raise Http404()
-#     return render(request,"index.html", {"image":image})
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"index.html", {"image":image})

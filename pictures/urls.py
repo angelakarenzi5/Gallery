c2e440dc341f57d5dd07d534ffa3b4f612d88e99
wecode <umurangamclaire@gmail.com>
2019-03-14 19:05:18 +0200

    adding datetime

--- pictures/urls.py
+++ pictures/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
-    url(r'^search/', views.search_results, name='search_results'),
+    # url(r'^search/', views.search_results, name='search_results'),
     url(r'^image/(\d+)',views.image,name ='image')
 ]
 if settings.DEBUG:
--- pictures/views.py
+++ pictures/views.py
@@ -1,25 +1,9 @@
 from django.shortcuts import render, redirect 
 from django.http import HttpResponse, Http404
+from datetime as dt 
 from .models import Image
 
 
-def search_results(request):
-
-    if 'image' in request.GET and request.GET["image"]:
-        search_term = request.GET.get("image")
-        searched_images = Image.search_by_title(search_term)
-        message = f"{search_term}"
-
-        return render(request, 'all-news/search.html',{"message":message,"images": searched_images})
-
-    else:
-        message = "You haven't searched for any term"
-        return render(request, 'search.html',{"message":message})
-
-def image(request,image_id):
-    
-    try:
-        image = Image.objects.get(id = image_id)
-    except DoesNotExist:
-        raise Http404()
-    return render(request,"index.html", {"image":image})
+def pictures_of_day(request):
+    date = dt.date.today()
+    return render(request, 'today-pictures.html', {"date": date,})

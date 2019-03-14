846a501cff811ae42cf2d35ad07321a953a8a89f
wecode <umurangamclaire@gmail.com>
2019-03-14 17:33:09 +0200

    creating database gallery

--- pictures/models.py
+++ pictures/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    city = models.CharField(max_length =60)
+    country = models.TextField()
+
+def search_by_title(cls,search_term):
+        pictures = cls.objects.filter(title__icontains=search_term)
+        return pictures
+
 
 # Create your models here.
 class Image(models.Model):
@@ -17,18 +31,4 @@ def save_editor(self):
 
 class Meta:
         ordering = ['image_name']
-
-class Category(models.Model):
-    name = models.CharField(max_length =30)
-
-    def __str__(self):
-        return self.name
-
-class Location(models.Model):
-    city = models.CharField(max_length =60)
-    country = models.TextField()
-
-def search_by_title(cls,search_term):
-        pictures = cls.objects.filter(title__icontains=search_term)
-        return pictures
 

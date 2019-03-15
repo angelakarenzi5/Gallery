95c62fc586402f09ab24ea5f4c4c771a443cc655
wecode <umurangamclaire@gmail.com>
2019-03-15 18:10:29 +0200

    Testing save method for Loction test

--- pictures/tests.py
+++ pictures/tests.py
@@ -6,3 +6,12 @@ class LocationTestClass(TestCase):
     # Set up method
     def setUp(self):
         self.wecode= Location(city = 'Kigali', country ='Rwanda')
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.wecode,Location))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.james.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
